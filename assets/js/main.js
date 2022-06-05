5066e9e5dca2971b4c481c3e527f0456b6d4d90a
Ryan Fitra Perdana <ryanfitraperdana@gmail.com>
2022-06-05 13:37:53 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,36 @@
 (function() {
   "use strict";
 
+  var countDownDate = new Date("June 6, 2022 00:00:00").getTime();
+  var x = setInterval(function(){
+    
+    var now = new Date().getTime();
+    var distance = countDownDate-now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if(distance = 0){
+      clearInterval(x);
+      document.querySelector("h1").innerText = "We Have Arrived!";
+      document.querySelector("p").innerHTML = "The big day is finally here - view our <a href=https://www.website.com>website<a/> for more information.";
+      const digit = document.querySelectorAll("span");
+      digit.forEach((digit) => {
+        digit.innerText = "0";
+      });
+    }
+
+   
+  },1000); 
+
+
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
